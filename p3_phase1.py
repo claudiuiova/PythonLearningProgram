c092fff87c2e79e835d42bcc8d1ca9183800a7a1
Claudiu Iova <claudiu.iova@tim-mb-155.3pillar.corp>
2018-03-01 18:09:45 +0200

    added p3 from phase1

--- p3_phase1.py
+++ p3_phase1.py
@@ -1,47 +1,95 @@
-def openFileAndGetTheInput():
-	with open("input_dict.txt") as dictFile:
-		return dictFile.read().split('\n')
+from collections import OrderedDict
 
-def createListWithLists():
-	inputs = openFileAndGetTheInput()
-	result = []
-	tempList = []
+class SortDicts:
 
-	for item in inputs:
-		if item == '':
-			result.append(tempList)
-			tempList = []
-		else:
-			tempList.append(item)
+	def __init__(self):
+		self.flag = True
+		self.key = 0
 
-	result.append(tempList)
-	return result
+	def openFileAndGetTheInput(self):
+		with open("input_dict.txt") as dictFile:
+			return dictFile.read().split('\n')
 
-def createListWithDictionaries():
-	inputs = createListWithLists()
-	dictList = []
+	def createListWithLists(self):
+		inputs = self.openFileAndGetTheInput()
+		result = []
+		tempList = []
 
-	for i in range(len(inputs)):
-		tempList = [{}]
+		for item in inputs:
+			if item == '':
+				result.append(tempList)
+				tempList = []
+			else:
+				tempList.append(item)
 
-		for item in inputs[i]:
-			item = item.split()
-			tempList[0].update({item[0]: int(item[1])})
-		
-		dictList.append({i+1: tempList[0]})
-	return dictList
+		result.append(tempList)
+		return result
+
+	def createListWithDictionaries(self):
+		inputs = self.createListWithLists()
+		dictList = []
+
+		for i in range(len(inputs)):
+			tempList = [{}]
+
+			for item in inputs[i]:
+				item = item.split()
+				tempList[0].update({item[0]: int(item[1])})
 			
-def sortDictionaries():
-	allDictionaries = createListWithDictionaries()
+			dictList.append({i+1: tempList[0]})
+		return dictList
 
-	for dictionary in allDictionaries:
-		for k,v in dictionary.items():
-			
-			print(min(v, key=v.get))
+	def sortDictionaryByKey(self, dictionary):
+		return dict(OrderedDict(sorted(dictionary.items())))
 
-			print('........')
-			
-			print(min(v))
 
-#need work
-sortDictionaries()
+	def sortAllDictionariesKeys(self):
+		sortedDicts = []
+		for dictionary in self.createListWithDictionaries():
+			for k,v in dictionary.items():
+				v = self.sortDictionaryByKey(v)
+				dictionary[k] = v
+				sortedDicts.append(dictionary)
+		return sortedDicts
+
+	def getValueFromDictionary(self, index, listWithDicts, key=0):
+		return listWithDicts[index][list(listWithDicts[index].keys())[0]][list(listWithDicts[index]
+			[list(listWithDicts[index].keys())[0]].keys())[key]]
+
+	def sortListWithDicts(self, listWithDicts):
+		for i in range(len(listWithDicts) - 1, 0, -1):
+			for j in range(i):
+				if self.getValueFromDictionary(j, listWithDicts) > self.getValueFromDictionary(j+1, listWithDicts):
+					temp = listWithDicts[j]
+					listWithDicts[j] = listWithDicts[j+1]
+					listWithDicts[j+1] = temp
+
+				elif self.getValueFromDictionary(j, listWithDicts) == self.getValueFromDictionary(j+1, listWithDicts):
+					while self.flag:
+						self.key += 1
+						if self.getValueFromDictionary(j, listWithDicts, key=self.key) > self.getValueFromDictionary(j+1, listWithDicts, key=self.key):
+							temp = listWithDicts[j]
+							listWithDicts[j] = listWithDicts[j+1]
+							listWithDicts[j+1] = temp
+							self.flag = False
+
+						if self.key == len(self.createListWithLists()[j])-1: #check also if the dictionaries are fully equal.
+							#if values are equal, the sorting algorithm will put the dictionaries how was inserted in the input file
+							break
+
+				self.flag = True
+				self.key = 0
+
+		return listWithDicts
+
+	def writeFile(self):
+		sortedList = self.sortListWithDicts(self.sortAllDictionariesKeys())
+		result = ''
+		for dict_ in sortedList:
+			result += "%s " %list(dict_.keys())[0]
+		with open("output_dict.txt", "w") as resultFile:
+			resultFile.write(str(result))
+
+if __name__ == '__main__':
+	sortObj = SortDicts()
+	sortObj.writeFile()
