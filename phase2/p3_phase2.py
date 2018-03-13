fca221f4db4ded81b484b5d305f9600c3dc26144
Claudiu Iova <claudiu.iova@tim-mb-155.3pillar.corp>
2018-03-13 11:29:04 +0200

    added p3_phase2.py

---
+++ phase2/p3_phase2.py
@@ -0,0 +1,44 @@
+"""
+P3. Write a function decorator that tracks how long the execution of 
+the wrapped function took. The decorator will log slow function calls including details 
+about the execution time and function name. The decorator should take an optional threshold argument.
+"""
+import time
+
+def arguments_dec(threshold = None):
+    def time_track(func):
+        def func_wrapper():
+            time_start = time.time()
+            func()
+            elapsed_time = time.time() - time_start
+            
+            if not callable(threshold):
+                if threshold > elapsed_time:
+                    return elapsed_time
+                else:
+                    print("Function execution took too long")
+                    return func()
+            else:
+                return elapsed_time
+            
+        return func_wrapper
+
+    if callable(threshold):
+        return time_track(threshold)
+    else:
+        return time_track
+
+@arguments_dec(threshold= 0.3)
+def my_func():
+    for _ in range(9999999):
+        pass
+    return ("Somtext")
+
+@arguments_dec
+def my_func1():
+    for _ in range(9999999):
+        pass
+    return ("Somtext1")
+
+print(my_func())
+print(my_func1())
