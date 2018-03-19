57bfcdeb55a5d7c022a65d661a4c81b7f84f5cba
Claudiu Iova <claudiu.iova@tim-mb-155.3pillar.corp>
2018-03-19 12:54:27 +0200

    p1_phase3 added

---
+++ phase3/p1_phase3.py
@@ -0,0 +1,28 @@
+"""
+P1. Given a binary tree encoded as a tuple (node_label, left_node_tuple, right_node_tuple) 
+write an iterator / generator that returns the nodes of the tree in pre-order.
+Example:
+Input: ('b', ('a', None, None), ('z', ('c', None, None), ('zz', None, None)))
+Output: b, a, z, c, zz
+
+"""
+
+def get_left_child(parent):
+    return parent[1]
+
+def get_right_child(parent):
+    return parent[2]
+
+def get_root_val(tree):
+    return tree[0]
+
+def traverse_in_preorder(tree):
+    if tree:
+        print(get_root_val(tree))
+        traverse_in_preorder(get_left_child(tree))
+        traverse_in_preorder(get_right_child(tree))
+
+if __name__ == '__main__':
+    tree = ('b', ('a', None, None), ('z', ('c', None, None), ('zz', None, None)))
+    traverse_in_preorder(tree)
+
